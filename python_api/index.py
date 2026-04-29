f3a23117adad79b0efcf93203311668fc521c529
Rudra <your-t23050288@gmail.com>
2026-04-29 11:54:01 +0530

    Fix: Disable CORS allow_credentials for wildcard support

--- python_api/index.py
+++ python_api/index.py
@@ -50,7 +50,7 @@ try:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
